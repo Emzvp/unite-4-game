7528b7cc77b5fe9e7251304f9e3735d0a752755a
Emily Betz <emilyebetz26@gmail.com>
2019-07-02 23:22:43 -0400

    javascript file updated, html updated, css created

---
+++ asset/javascript/game.js
@@ -0,0 +1,103 @@
+
+
+$(document).ready(() => {
+
+   
+    let randNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+    $('#randNum').html(randNumber);
+
+    
+    let white = Math.floor(Math.random() * 17) + 1;
+    let lightBlue = Math.floor(Math.random() * 17) + 1;
+    let blue = Math.floor(Math.random() * 17) + 1;
+    let purple = Math.floor(Math.random() * 17) + 1;
+
+    
+    let wins = 0; 
+    let losses = 0;  
+
+    let totalScore = 0;
+    $('#totalScore').text(totalScore);
+
+    function win() {
+        wins = wins + 1;
+        $('#wins').text(wins);
+        resetTwo();
+        reset();
+    }
+
+    function lose() {
+        losses = losses + 1;
+        $('#losses').text(losses);
+        resetTwo();
+        reset();
+    }
+
+    $('#Crystal1').click(() => {
+        totalScore = totalScore + white
+        $('#totalScore').text(totalScore);
+        if (totalScore === randNumber) {
+            win();
+            reset();
+            resetTwo();
+        } else if (totalScore > randNumber) {
+            lose();
+            reset();
+            resetTwo();
+        }
+    });
+
+    $('#Crystal2').click(() => {
+        totalScore = totalScore + lightBlue;
+        $('#totalScore').text(totalScore);
+        if (totalScore === randNumber) {
+            win();
+            reset();
+          
+        } else if (totalScore > randNumber) {
+            lose();
+            reset();
+            
+        }
+    });
+
+    $('#Crystal3').click(() => {
+        totalScore = totalScore + blue;
+        $('#totalScore').text(totalScore);
+        if (totalScore === randNumber) {
+            win();
+            reset();
+            
+        } else if (totalScore > randNumber) {
+            lose();
+            reset();
+            
+        }
+    });
+
+    $('#Crystal4').click(() => {
+        totalScore = totalScore + purple;
+        $('#totalScore').text(totalScore);
+        if (totalScore === randNumber) {
+            win();
+            reset();
+           
+        } else if (totalScore > randNumber) {
+            lose();
+            reset();
+           
+        }
+    });
+    function reset() {
+        totalScore = 0;
+        $('#totalScore').text(totalScore);
+        if (totalScore===randNumber||totalScore>randNumber){
+            white = Math.floor(Math.random() * 17) + 1;
+            lightBlue = Math.floor(Math.random() * 17) + 1;
+            blue = Math.floor(Math.random() * 17) + 1;
+             purple = Math.floor(Math.random() * 17) + 1;
+
+        }
+    }
+
+});
